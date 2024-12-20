e553573f1fbd5701fe4285e45ba76907d5b0b274
andrewjamesmoore <andrew.moore@samdesk.io>
2024-12-19 23:41:17 -0700

    feat: color theme toggle

--- app.py
+++ app.py
@@ -9,17 +9,13 @@ def home():
     with open('./data/projects.json') as f:
         project_list = json.load(f)
         theme = request.cookies.get("theme", "light-mode")
+        current_date = datetime.now().strftime("%B %Y")
         if not theme:
             prefers_dark = "dark" in request.headers.get("User-Agent", "").lower()
             theme = "dark-mode" if prefers_dark else "light-mode"
-        current_date = datetime.now().strftime("%B %d, %Y")
     return render_template("index.html", projects=project_list, date=current_date, theme=theme)
 
-@app.route("/about")
-def about():
-    return render_template("about.html")
-
-@app.route("/toggle_theme", methods=["POST"])
+@app.route("/toggle_theme", methods=["GET", "POST"])
 def toggle_theme():
     current_theme = request.cookies.get("theme", "light-mode")
     new_theme = "dark-mode" if current_theme == "light-mode" else "light-mode"
